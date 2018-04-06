d68da62b6344ed734d391cf19d7b3aaad7cc41d3
jongleberry <me@jongleberry.com>
2018-04-06 15:03:25 -0400

    automation: add solution for automation test

--- tests/automation/home.js
+++ tests/automation/home.js
@@ -1,5 +1,6 @@
 
 const { By, until } = require('selenium-webdriver')
+const Input = require('selenium-webdriver/lib/input')
 
 const { domain } = require('../config')
 
@@ -8,9 +9,9 @@ Submit text to the form.
 `
 
 exports.options = {}
-exports.parameters = {
+exports.parameters = [{
   domain
-}
+}]
 
 exports.test = ({
   driver,
@@ -21,14 +22,25 @@ exports.test = ({
 
   step('Visit the page.', async () => {
     await driver.get(domain)
-    await driver.wait(until.urlContains(domain))
+  }, {
+    timeout: '5s'
   })
 
   step('Submit text.', async () => {
-    /* const input = */ await driver.wait(until.elementLocated(By.css('input')))
+    const input = await driver.wait(until.elementLocated(By.css('input')))
+    // const input = await driver.wait(driver.findElement(By.css('input')))
+    await input.sendKeys('asdf')
+    await input.sendKeys(Input.Key.RETURN)
+
+    // const button = await driver.wait(until.elementLocated(By.css('button')))
+    // button.click()
+  }, {
+    timeout: '1s'
   })
 
   step('Expect a result.', async () => {
-
+    await driver.wait(until.elementLocated(By.css('table tbody tr')))
+  }, {
+    timeout: '1s'
   })
 }
